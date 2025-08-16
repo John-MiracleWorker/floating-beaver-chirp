c94ee0cab18edaf415a39f27c383d0c830e14c12
[dyad] <git@dyad.sh>
2025-08-16 17:41:18 -0400

    [dyad] Fixing React render mismatch in entry file - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error('Root element "#root" not found');
+
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
