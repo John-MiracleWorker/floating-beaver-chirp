49c270942e7fe2961b72023f42fa014a505f51df
[dyad] <tiuni65@gmail.com>
2025-08-16 18:40:57 -0400

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./globals.css";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error('Root element "#root" not found');
 
-ReactDOM.createRoot(rootEl).render(
+createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
