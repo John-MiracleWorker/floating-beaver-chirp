7bcd168d554fe993415369a51801493dde9e9bfb
[dyad] <tiuni65@gmail.com>
2025-08-16 21:37:45 -0400

    [dyad] Removed legacy Leaflet imports and code from main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./globals.css";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-// Fix Leaflet’s default icon URLs
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error('Root element "#root" not found');
