3d4669e2509b8def6e3f193c2af0ac94596006da
[dyad] <tiuni65@gmail.com>
2025-08-16 21:38:45 -0400

    [dyad] Fixed Vite resolution by adding regex aliases for react and react-dom/client - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "./src") },
       { find: "react/jsx-dev-runtime", replacement: "react/jsx-runtime" },
+      { find: /^react$/, replacement: path.resolve(__dirname, "node_modules/react") },
+      { find: /^react-dom\/client$/, replacement: path.resolve(__dirname, "node_modules/react-dom/client.js") },
     ],
   },
 });
