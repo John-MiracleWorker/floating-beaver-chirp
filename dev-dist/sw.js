272ea548f8dbebcfeecc11bdaa4e30d02e09c783
[dyad] <tiuni65@gmail.com>
2025-08-16 22:53:06 -0400

    [dyad] Updated eslint-plugin-react-hooks and vite versions to resolve lockfile mismatch - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tfdkf7i3ak8"
+    "revision": "0.s4d84cblrd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
